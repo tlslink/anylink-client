93e22413f215750239390a0f3be6aee60aabb0c9
Xinjun Ma <xinjun.ma@qq.com>
2024-01-21 11:27:00 +0800

    bump version

--- anylink.pro
+++ anylink.pro
@@ -43,7 +43,7 @@ defineTest(copyIcon) {
     export(QMAKE_POST_LINK)
 }
 
-VERSION = 0.9.3
+VERSION = 0.9.4
 
 macx {
     HEADERS += src/macdockiconhandler.h
--- src/common.cpp
+++ src/common.cpp
@@ -4,7 +4,7 @@
 
 QString agentName = "AnyLink Secure Client";
 
-QString appVersion = "0.9.3";
+QString appVersion = "0.9.4";
 
 QString configLocation = "";
 QString tempLocation = "";
